f872bec37e2fa3e1b8d9e3a2618e88f55a7ffdc6
Jake Morris <jmorr32@uwo.ca>
2017-10-18 16:21:00 -0400

    Rough in modifyObject

---
+++ midpoint-client-api/src/main/java/com/evolveum/midpoint/client/api/ObjectModifyService.java
@@ -0,0 +1,12 @@
+package com.evolveum.midpoint.client.api;
+
+/**
+ * Description
+ *
+ * @author Jake Morris - jake
+ * @version 1.0
+ * @since 1.0
+ */
+public class ObjectModifyService
+{
+}
---
+++ midpoint-client-impl-rest-jaxb/src/main/java/com/evolveum/midpoint/client/impl/restjaxb/RestJaxbObjectModifyService.java
@@ -0,0 +1,12 @@
+package com.evolveum.midpoint.client.impl.restjaxb;
+
+/**
+ * Description
+ *
+ * @author Jake Morris - jake
+ * @version 1.0
+ * @since 1.0
+ */
+public class RestJaxbObjectModifyService
+{
+}
